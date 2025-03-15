87de2ebf6c9ba3a79e0d67458c12ab2b1a2f949c
Tashini4 <tashiniwihethunaga@gmail.com>
2025-03-15 22:12:39 +0530

    first commit

---
+++ src/main/java/lk/ijse/aad_coursework/AadCourseworkApplication.java
@@ -0,0 +1,12 @@
+package lk.ijse.aad_coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadCourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AadCourseworkApplication.class, args);
+    }
+}
---
+++ src/test/java/lk/ijse/aad_coursework/AadCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad_coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadCourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
